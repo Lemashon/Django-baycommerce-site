2f5bee48a46376e5ab8c8e2b9da2f74ae29bbd40
Lemashon <lemakipeno@gmail.com>
2022-12-23 12:29:05 +0300

    second commit

--- baycommerce/app/urls.py
+++ baycommerce/app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from . forms import LoginForm
 
 urlpatterns = [
     path('', views.home),
@@ -14,5 +16,8 @@ urlpatterns = [
     
     #login aunthnticarion
    path('registration/', views.CustomerRegistrationView.as_view(),name="customerregistration"),
+   path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm),name="login"),
+   path('password-reset', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm),
+    name="password_reset"),
     
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- baycommerce/app/views.py
+++ baycommerce/app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from . models import Product
 from . forms import CustomerRegistrationForm
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
@@ -33,3 +35,14 @@ class CustomerRegistrationView(View):
     def get(self, request):
         form = CustomerRegistrationForm()
         return render(request, "app/customerregistration.html", locals())
+    
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Congratulations! User Registered Successfully!")
+        else:
+            messages.warning(request, "Invalid Input Data")
+        return render(request, 'app/customerregistration.html', locals())
+    
+    
